037cce505c06230aa2fd0b82a071babf5ecc604d
cry <113723497+f213u47gj@users.noreply.github.com>
2024-01-20 06:19:17 +0300

    Add files via upload

---
+++ PR19/PR19/PR19.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PR19 //  имя простртранства 
+{
+    public class PR19 // создание класса 
+    {
+        public void ProductionOfOddNumbers() // создаем метод
+        {
+            int result = 1; // задали перемнную результата
+            int sum = 0;
+            int proiz = 1;
+
+            for (int i = 11; i < 100; i += 2) // создали цикл  от 13 до 99, где шаг 26
+            {
+                if (i % 13 == 0) // проверка на кратность 13
+                {
+                    proiz *= i; // произведение
+                    result = i*i; // умножение друг на друга
+                    sum += result; // считаем сумму
+                    Console.WriteLine($"Произведение {i}*{i}=" + result); // Вывод результата
+                }
+            }
+            Console.WriteLine("Сумма всех произвдений: " + sum ); // Вывод результата
+            Console.WriteLine("Произведение чисел: " + proiz); // Вывод результата
+        }
+    } 
+}
+    
+
---
+++ PR19/PR19/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PR19
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            PR19 pr19 = new PR19();
+            pr19.ProductionOfOddNumbers();
+        }
+    }
+}
+// Ответы на тест
+// 1. a,b,c,d,g
+// 2. b,g
+// 3. b
+// 4. a, e
+// 5. b, d
+// 6. f 
+// 7. a
+// 8. b, d
+// 9. a
+// 10. a,c,e,g
+// 11. d
+// 12. c
+// 13. b
+// 14. b
+// 15. c
+// 16. b
